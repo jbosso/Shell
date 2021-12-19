04e10f66231035ece9efc2fa1d55347c2c90a435
jbosso <j_bosso@outlook.com>
2021-12-19 02:28:40 +0000

    message

--- exec/deploy.sh
+++ exec/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-uri='sudo ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@52.67.249.96'
+uri='ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@52.67.249.96'
 
 export TF_VAR_vpcId=$($uri 'sudo aws --region sa-east-1 ec2 describe-vpcs --filters Name=tag:Name,Values=vpc-Team4-JB --query "Vpcs[*].VpcId" --output text')
 export TF_VAR_subnetPrivada=$($uri 'sudo aws --region sa-east-1 ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-0f50e5ca99893dcc9" "Name=tag:Name,Values=*Priv*" --query "Subnets[1].SubnetId" --output text')
