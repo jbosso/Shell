99e67a47f878f339c18931231e6193cc4638f06a
jbosso <j_bosso@outlook.com>
2021-12-19 04:49:03 +0000

    message

---
+++ 05-AppDeploy/deploydev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd 05-AppDeploy/ansible
+
+export masterIP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=k8s-master0 --query "Reservations[*].Instances[*].[PrivateIpAddress]" --output text)
+
+
+echo "[k8s-master0]" > ../ansible/hosts
+echo $masterIP >> ../ansible/hosts 
+
+
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarDev.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
