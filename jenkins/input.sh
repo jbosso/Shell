8208cb7e4c939498910c133afce391694b02bf84
jbosso <j_bosso@outlook.com>
2021-12-20 01:50:06 +0000

    message

--- jenkins/input.sh
+++ jenkins/input.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DB_PASSWORD='root'
 
-env.DB_PASSWORD=bat(returnStdout: true, 
-script: '''  @echo off echo '$DB_PASSWORD' ''').trim()
+env.ABC=bat(returnStdout: true, 
+script: '''  @echo off echo abc ''').trim()
